ad90b7ae99d9ffd8679e81adb5b889dc3a698d47
daveiator <43887102+daveiator@users.noreply.github.com>
2023-03-10 15:59:31 +0100

    implemented logger

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ use socket2::{Domain, Socket, Type, Protocol};
 
 use local_ip_address::local_ip;
 
+use simple_logger::SimpleLogger;
+use log::{info, debug, warn, error};
+
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -54,10 +57,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         },
 
         Command::Default(args) => {
-            println!("Checking for device named \"{}\"...", args.device_name);
+            log_panics::init();
+            SimpleLogger::new()
+                .with_level(match args.options.verbose {
+                    true => log::LevelFilter::Debug,
+                    false => log::LevelFilter::Info,
+                })
+                .without_timestamps()
+                .with_colors(true)
+                .init()?;
+
+
+            info!("Checking for device named \"{}\"...", args.device_name);
             let ports= match available_ports() {
                 Err(error) => {
-                    eprintln!("Coulnd't get available ports list: {}", error);
+                    error!("Coulnd't get available ports list: {}", error);
                     eprintln!("Exiting...");
                     std::process::exit(1);
                 },
@@ -66,55 +80,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut device = "";
             for port in ports {
                 if port.port_name == args.device_name {
-                    println!("Found device \"{}\"", args.device_name);
+                    info!("Found device \"{}\"", args.device_name);
                     device = &args.device_name;
                     break;
                 }
             }
             if device == "" {
-                eprintln!("Couldn't find device named \"{}\"", args.device_name);
+                error!("Couldn't find device named \"{}\"", args.device_name);
                 eprintln!("Exiting...");
                 std::process::exit(1);
             }
-            println!("Starting dmx interface...");
+            info!("Starting dmx interface...");
             let mut dmx = match DMXSerial::open_sync(device) {
                 Ok(dmx) => dmx,
                 Err(error) => {
-                    eprintln!("Couldn't open dmx interface: {}", error);
+                    error!("Couldn't open dmx interface: {}", error);
                     eprintln!("Exiting...");
                     std::process::exit(1);
                 },
             };
             if args.options.remember {
+                debug!("Setting dmx interface to remember mode");
                 dmx.set_async();
             }
-            println!("Started!");
-            println!("Starting art-net listener...");
+            info!("Started!");
+            info!("Starting art-net listener...");
             let address: SocketAddr = format!("{}:{}", args.options.controller.clone().unwrap_or("0.0.0.0".into()), args.options.port.unwrap_or(6454)).parse()?;
             let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
             socket.set_reuse_address(true)?;
             // socket.set_nonblocking(true)?;
+            debug!("Binding socket to address: {}:{}", address, args.options.port.unwrap_or(6454));
             socket.bind(&address.into())?;
             let socket: UdpSocket = socket.into();
 
-            println!("Started!");
+            info! ("Started!");
             let mut buffer = [0; 1024];
             loop {
                 let (length, controller) = match socket.recv_from(&mut buffer) {
                     Ok(x) => x,
                     Err(error) => {
-                        if args.options.verbose {
-                            eprintln!("Couldn't receive art-net packet: {}", error);
-                        }
+                        warn!("Couldn't receive art-net packet: {}", error);
                         continue;
                     },
                 };
                 let command = match ArtCommand::from_buffer(&buffer[..length]) {
                     Ok(command) => command,
                     Err(error) => {
-                        if args.options.verbose {
-                            eprintln!("Couldn't parse art-net packet: {}", error);
-                        }
+                        warn!("Couldn't parse art-net packet: {}", error);
                         continue;
                     },
                 };
@@ -123,14 +135,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 match command {
                     ArtCommand::Poll(_) => {
-                        println!("Received Poll");
+                        debug!("Received Poll");
                         if !local_ip().is_ok_and(|ip| ip.is_ipv4()) {
-                            eprintln!("Can't reply to poll request: No IPv4 address found");
+                            warn!("Can't reply to poll request: No IPv4 address found");
                             continue;
                         }
-                        if args.options.verbose {
-                            println!("Preparing PollReply");
-                        }
+                        debug!("Preparing PollReply");
                         let address = Ipv4Addr::from_str(local_ip().unwrap().to_string().as_str()).unwrap();
                         let mut short_name = [0; 18];
                         "artnet2opendmx".bytes().enumerate().for_each(|(i, b)| short_name[i] = b);
@@ -175,36 +185,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         let reply_bytes = match ArtCommand::PollReply(Box::new(reply)).write_to_buffer() {
                             Ok(bytes) => bytes,
                             Err(error) => {
-                                eprintln!("Couldn't write poll reply: {}", error);
+                                warn!("Couldn't write poll reply: {}", error);
                                 continue;
                             },
                         };
-                        if args.options.verbose {
-                            println!("Sending poll reply to {}", controller);
-                        }
+                        debug!("Sending poll reply to {}", controller);
                         match socket.send_to(&reply_bytes, controller) {
                             Ok(_) => {},
                             Err(error) => {
-                                eprintln!("Couldn't send poll reply: {}", error);
+                                warn!("Couldn't send poll reply: {}", error);
                                 continue;
                             },
                         }
                     },
                     ArtCommand::Output(output) => {
                         if output.port_address == PortAddress::try_from(args.universe).unwrap() {
-                            if args.options.verbose {
-                                println!("Received output for universe {} from controller {}", args.universe, controller);
-                            }
+                            debug!("Received output for universe {} from controller {}", args.universe, controller);
                             let mut channels = [0; 512];
                             _ = output.to_bytes()?[8..].iter().zip(channels.iter_mut()).for_each(|(a, b)| *b = *a);
                             dmx.set_channels(channels);
-                                dmx.update();
+                            dmx.update();
+                            debug!("Updated dmx channels on interface");
                         }
                     },
+                    ArtCommand::PollReply(_) => {}, // Ignore
                     _ => {
-                        // if args.options.verbose {
-                        //     println!("Received art-net packet: {:?}", x);
-                        // }
+                        debug!("Received unimplemented art-net packet, disregarding...");
                     }
                 }
             }
